bdfede9d04cdcae3402fbfcb51e1fd0c8d9fc945
Jinghua.Yao <727004611@qq.com>
2019-01-24 15:34:21 +0800

    add 222

--- cpp/222.cc
+++ cpp/222.cc
@@ -1,5 +1,5 @@
 /*
- *
+ * https://leetcode.com/problems/count-complete-tree-nodes/
  */
 #include <iostream>
 #include <vector>
@@ -12,3 +12,29 @@
 #include <set>
 #include <deque>
 #include <queue>
+
+
+using namespace std;
+
+
+
+//Definition for a binary tree node.
+//struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//};
+//
+//class Solution {
+//public:
+//	int countNodes(TreeNode* root) {
+//		if (root == nullptr) {
+//			return 0;
+//		}
+//		int res = 1;		
+//		res += countNodes(root->left);
+//		res += countNodes(root->right);
+//		return res;
+//	}
+//};
